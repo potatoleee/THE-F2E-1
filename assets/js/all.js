b1541fe550c9ff23e382e06cd9f38bbaaa2f1515
potatolee <dean19990226@gmail.com>
2022-11-10 01:05:31 +0800

    第二區

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,7 +4,29 @@ import { gsap, ScrollTrigger, TextPlugin } from "../../node_modules/gsap/all.js"
 // import { gsap } from "../../node_modules/gsap";
 // import { ScrollTrigger } from "../../node_modules/gsap/ScrollTrigger";
 // import { TextPlugin } from "../../node_modules/gsap/TextPlugin";
+AOS.init();
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
 
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 gsap.to(".loop", {
@@ -54,4 +76,5 @@ tl.to('.box1', {
     scale: 1.1,
     speed: 1000,
   });
+
   
